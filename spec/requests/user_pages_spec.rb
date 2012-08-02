69533921ea7276f73eaf1841e2ee3e01ac38e675
trumbitta <trumbitta@gmail.com>
2012-08-02 19:38:03 +0200

    refactor tests to decouple the tests from the implementation

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,8 +8,7 @@ describe "User pages" do
 	let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
-    it { should have_selector('h1',    text: user.name) }
-    it { should have_selector('title', text: user.name) }
+    it { should be_page_by_title(user.name) }
   end
 
   describe "signup" do
@@ -17,8 +16,7 @@ describe "User pages" do
       before { visit signup_path }
       let(:submit) { "Create my account" }
 
-      it { should have_selector('h1',    text: 'Sign up') }
-      it { should have_selector('title', text: full_title('Sign up')) }
+      it { should be_page_by_title('Sign up') }
 
       describe "with invalid information" do
         it "should not create a user" do
@@ -28,7 +26,7 @@ describe "User pages" do
         describe "after submission" do
           before { click_button submit }
 
-          it { should have_selector('title', text: 'Sign up') }
+          it { should have_doc_title('Sign up') }
           it { should have_content('error') }
         end
       end
@@ -49,8 +47,8 @@ describe "User pages" do
           before { click_button submit }
           let(:user) { User.find_by_email('user@example.com') }
 
-          it { should have_selector('title', text: user.name) }
-          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+          it { should have_doc_title(user.name) }
+          it { should have_success_message('Welcome') }
           it { should have_link('Sign out') }
         end
       end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,38 @@
 include ApplicationHelper
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_doc_title do |title|
+  match do |page|
+    page.should have_selector('title', text: full_title(title))
+  end
+end
+
+RSpec::Matchers.define :have_content_title do |title|
+  match do |page|
+      page.should have_selector('h1', text: title)
+  end
+end
+
+RSpec::Matchers.define :be_page_by_title do |title|
+  match do |page|
+      page.should have_doc_title(title)
+      page.should have_content_title(title)
+  end
+end
