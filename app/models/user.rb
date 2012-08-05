6d3531a18ce062b0b75e100e36c6d68e0b4f7333
trumbitta <trumbitta@gmail.com>
2012-08-05 20:19:29 +0200

    till chapter 10.3.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
-    has_secure_password
+  has_secure_password
 
   before_save { self.email.downcase! }
   before_save :create_remember_token
@@ -24,6 +24,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  has_many :microposts, dependent: :destroy
 
   private
 
